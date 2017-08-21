4c2ff2f5b58c2578ae17194b4545aa3ee7915896
Qiwen Zhu <qiwzhu@indiana.edu>
2017-08-21 00:17:14 -0400

    work on internship application form wizard

--- resources/views/layouts/partials/default/content/services_cards.blade.php
+++ resources/views/layouts/partials/default/content/services_cards.blade.php
@@ -4,7 +4,7 @@
     <div class="text-center">
         <h3 class="border-primary"><span class="heading_border bg-primary">What you can do with the us</span></h3>
     </div>
-    <div class="col-sm-6 col-md-3 wow bounceInLeft" data-wow-duration="0.5s">
+    <div class="col-sm-12 col-md-4 wow bounceInLeft" data-wow-duration="0.5s">
         <div class="box">
             <a href="#">
                 <div class="box-icon">
@@ -23,7 +23,7 @@
     </div>
     <!-- //Responsive Section End -->
     <!-- Easy to Use Section Start -->
-    <div class="col-sm-6 col-md-3 wow bounceInDown" data-wow-duration="0.5s" data-wow-delay="0.1s">
+    <div class="col-sm-12 col-md-4 wow bounceInDown" data-wow-duration="0.5s" data-wow-delay="0.1s">
         <!-- Box Start -->
         <div class="box">
             <a href="#">
@@ -45,27 +45,27 @@
     <!-- //Easy to Use Section End -->
     <!-- Clean Design Section Start -->
     <!-- only alumni role can see this -->
-    <div class="col-sm-6 col-md-3 wow bounceInUp" data-wow-duration="0.5s" data-wow-delay="0.1s">
-        <div class="box">
-            <a href="#">
-                <div class="box-icon box-icon2">
-                    <i class="livicon icon1" data-name="doc-portrait" data-size="55" data-loop="true" data-c="#f89a14" data-hc="#f89a14"></i>
-                </div>
-                <div class="info">
-                    <h3 class="warning text-center">Alumni</h3>
-                    <p>Get connected with others</p>
-                    <p>Apply to be a SGIS alumnus</p>
-                    <p>Learn how we help our students</p>
-                    <div class="text-right primary"><a href="#">Read more</a>
-                    </div>
-                </div>
-            </a>
+    {{--<div class="col-sm-6 col-md-3 wow bounceInUp" data-wow-duration="0.5s" data-wow-delay="0.1s">--}}
+        {{--<div class="box">--}}
+            {{--<a href="#">--}}
+                {{--<div class="box-icon box-icon2">--}}
+                    {{--<i class="livicon icon1" data-name="doc-portrait" data-size="55" data-loop="true" data-c="#f89a14" data-hc="#f89a14"></i>--}}
+                {{--</div>--}}
+                {{--<div class="info">--}}
+                    {{--<h3 class="warning text-center">Alumni</h3>--}}
+                    {{--<p>Get connected with others</p>--}}
+                    {{--<p>Apply to be a SGIS alumnus</p>--}}
+                    {{--<p>Learn how we help our students</p>--}}
+                    {{--<div class="text-right primary"><a href="#">Read more</a>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</a>--}}
 
-        </div>
-    </div>
+        {{--</div>--}}
+    {{--</div>--}}
     <!-- //Clean Design Section End -->
     <!-- 20+ Page Section Start -->
-    <div class="col-sm-6 col-md-3 wow bounceInRight" data-wow-duration="0.5s" data-wow-delay="0.1s">
+    <div class="col-sm-12 col-md-4 wow bounceInRight" data-wow-duration="0.5s" data-wow-delay="0.1s">
         <div class="box">
             <a href="#">
                 <div class="box-icon box-icon3">
