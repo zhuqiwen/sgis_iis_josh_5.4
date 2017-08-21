7c7eb01f5e80e84001c6b62c9590877d37f0685a
qiwzhu <qiwzhu@oah.org>
2017-08-21 13:00:21 -0400

    fix bug in internship application layout

--- resources/views/frontend/partials/internship_application_form/organization.blade.php
+++ resources/views/frontend/partials/internship_application_form/organization.blade.php
@@ -8,28 +8,28 @@ $organization_types = [
 ?>
 <div class="tab-pane" id="tab1">
     <div class="row">
-        <div class="col-sm-12 col-md-12">
+        <div class="col-sm-12 col-md-10 col-md-offset-1">
             <h4 class="info-text"><b>Which organization</b> is this internship with?</h4>
         </div>
-        <div class="col-sm-12 col-md-5 col-md-offset-1">
+        <div class="col-sm-12 col-md-10 col-md-offset-1">
             <div class="form-group">
-                {!! Form::label('name', 'Organization name') !!}
-                {!! Form::text('name', 'Please do not use any abbreviation, such as IBM', ['id' => 'input-org-name','class' => 'form-control']) !!}
+                {!! Form::label('intern_organization_name', 'Organization name') !!}
+                {!! Form::text('intern_organization_name', 'Please do not use any abbreviation, such as IBM', ['id' => 'input-org-name','class' => 'form-control']) !!}
 
             </div>
         </div>
-        <div class="col-sm-12 col-md-5 col-md-offset-1">
+        <div class="col-sm-12 col-md-10 col-md-offset-1">
             <div class="form-group">
                 {{--here need to check if the url is legal--}}
-                {!! Form::label('url', 'Organization Website URL') !!}
-                {!! Form::text('url', 'https://', ['id' => 'input-org-url', 'class' => 'form-control']) !!}
+                {!! Form::label('intern_organization_url', 'Organization Website URL') !!}
+                {!! Form::text('intern_organization_url', 'https://', ['id' => 'input-org-url', 'class' => 'form-control']) !!}
                 <br>
             </div>
         </div>
-        <div class="col-sm-12 col-md-5 col-md-offset-1">
+        <div class="col-sm-12 col-md-10 col-md-offset-1">
             <div class="form-group">
-                {!! Form::label('type', 'What type the organization is?') !!}
-                {!! Form::select('type', $organization_types, NULL, ['class' => 'form-control']) !!}
+                {!! Form::label('intern_organization_type', 'What type the organization is?') !!}
+                {!! Form::select('intern_organization_type', $organization_types, NULL, ['class' => 'form-control']) !!}
                 <br>
             </div>
         </div>
