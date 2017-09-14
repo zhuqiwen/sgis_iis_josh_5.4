9a67df9609d5ae9f44901c28471c48e772db1c96
Qiwen Zhu <qiwzhu@indiana.edu>
2017-09-14 16:59:29 -0400

    minor change in text

--- resources/views/admin/layouts/partials/left_menu_items/scholarship.blade.php
+++ resources/views/admin/layouts/partials/left_menu_items/scholarship.blade.php
@@ -2,7 +2,7 @@
     <a href="#">
         <i class="livicon" data-name="medal" data-size="18" data-c="#6CC66C" data-hc="#6CC66C"
            data-loop="true"></i>
-        <span class="title">Overseas Study Funding</span>
+        <span class="title">Scholarships</span>
         <span class="fa arrow"></span>
     </a>
     <ul class="sub-menu">
