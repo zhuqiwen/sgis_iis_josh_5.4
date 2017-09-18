ef9b05f5855fa300d98d7eae8058d11e89e795e2
qiwzhu <qiwzhu@oah.org>
2017-09-18 14:07:16 -0400

    fix bug

--- resources/views/admin/layouts/default.blade.php
+++ resources/views/admin/layouts/default.blade.php
@@ -49,7 +49,7 @@
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         @if(Sentinel::getUser()->pic)
 {{--                            <img src="{!! url('/').'/uploads/users/'.Sentinel::getUser()->pic !!}" alt="img" height="35px" width="35px"--}}
-                            <img src="{!! asset('assets/img/authors/').Sentinel::getUser()->pic !!}" alt="img" height="35px" width="35px"
+                            <img src="{!! asset('assets/img/authors').'/'.Sentinel::getUser()->pic !!}" alt="img" height="35px" width="35px"
                                  class="img-circle img-responsive pull-left"/>
                         @else
                             <img src="{!! asset('assets/img/authors/avatar3.jpg') !!} " width="35"
