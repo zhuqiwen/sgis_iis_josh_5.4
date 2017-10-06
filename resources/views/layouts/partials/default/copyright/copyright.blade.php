bfcd0dd10f3b8137e81b76311f4e07b46b1c2975
Qiwen Zhu <qiwzhu@indiana.edu>
2017-10-06 00:55:08 -0400

    change footer

--- resources/views/layouts/partials/default/copyright/copyright.blade.php
+++ resources/views/layouts/partials/default/copyright/copyright.blade.php
@@ -1,5 +1,6 @@
-<div class="copyright">
+{{--<div class="copyright">--}}
+<div class="icon-section">
     <div class="container">
-        <p>SGIS, IUB, 2017</p>
+        {{--<p>SGIS, IUB, 2017</p>--}}
     </div>
 </div>
