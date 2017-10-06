1e882f4cad02006a6c1036b47c8fe6e8463ffe3b
Qiwen Zhu <qiwzhu@indiana.edu>
2017-10-06 14:44:51 -0400

    disable visitors map in admin index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -357,7 +357,7 @@
         </div>
         <div class="clearfix"></div>
         <div class="row ">
-            <div class="col-md-4 col-sm-12">
+            <div class="col-md-12 col-sm-12">
                 <div class="panel panel-danger">
                     <div class="panel-heading border-light">
                         <h4 class="panel-title">
@@ -391,55 +391,55 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-8 col-md-8 col-sm-12">
-                <div class="panel panel-border">
+            {{--<div class="col-lg-8 col-md-8 col-sm-12">--}}
+                {{--<div class="panel panel-border">--}}
 
-                    <div class="panel-heading">
-                        <h4 class="panel-title pull-left margin-top-10">
-                            <i class="livicon" data-name="map" data-size="16" data-loop="true" data-c="#515763"
-                               data-hc="#515763"></i>
-                            Visitors Map
-                        </h4>
+                    {{--<div class="panel-heading">--}}
+                        {{--<h4 class="panel-title pull-left margin-top-10">--}}
+                            {{--<i class="livicon" data-name="map" data-size="16" data-loop="true" data-c="#515763"--}}
+                               {{--data-hc="#515763"></i>--}}
+                            {{--Visitors Map--}}
+                        {{--</h4>--}}
 
-                        <div class="btn-group pull-right">
-                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                                <i class="livicon" data-name="settings" data-size="16" data-loop="true" data-c="#515763"
-                                   data-hc="#515763"></i>
-                            </button>
-                            <ul class="dropdown-menu" role="menu">
-                                <li>
-                                    <a class="panel-collapse collapses" href="#">
-                                        <i class="fa fa-angle-up"></i>
-                                        <span>Collapse</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a class="panel-refresh" href="#">
-                                        <i class="fa fa-refresh"></i>
-                                        <span>Refresh</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a class="panel-config" href="#panel-config" data-toggle="modal">
-                                        <i class="fa fa-wrench"></i>
-                                        <span>Configurations</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a class="panel-expand" href="#">
-                                        <i class="fa fa-expand"></i>
-                                        <span>Fullscreen</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
+                        {{--<div class="btn-group pull-right">--}}
+                            {{--<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">--}}
+                                {{--<i class="livicon" data-name="settings" data-size="16" data-loop="true" data-c="#515763"--}}
+                                   {{--data-hc="#515763"></i>--}}
+                            {{--</button>--}}
+                            {{--<ul class="dropdown-menu" role="menu">--}}
+                                {{--<li>--}}
+                                    {{--<a class="panel-collapse collapses" href="#">--}}
+                                        {{--<i class="fa fa-angle-up"></i>--}}
+                                        {{--<span>Collapse</span>--}}
+                                    {{--</a>--}}
+                                {{--</li>--}}
+                                {{--<li>--}}
+                                    {{--<a class="panel-refresh" href="#">--}}
+                                        {{--<i class="fa fa-refresh"></i>--}}
+                                        {{--<span>Refresh</span>--}}
+                                    {{--</a>--}}
+                                {{--</li>--}}
+                                {{--<li>--}}
+                                    {{--<a class="panel-config" href="#panel-config" data-toggle="modal">--}}
+                                        {{--<i class="fa fa-wrench"></i>--}}
+                                        {{--<span>Configurations</span>--}}
+                                    {{--</a>--}}
+                                {{--</li>--}}
+                                {{--<li>--}}
+                                    {{--<a class="panel-expand" href="#">--}}
+                                        {{--<i class="fa fa-expand"></i>--}}
+                                        {{--<span>Fullscreen</span>--}}
+                                    {{--</a>--}}
+                                {{--</li>--}}
+                            {{--</ul>--}}
+                        {{--</div>--}}
 
-                    </div>
-                    <div class="panel-body nopadmar">
-                        <div id="world-map-markers" style="width:100%; height:300px;"></div>
-                    </div>
-                </div>
-            </div>
+                    {{--</div>--}}
+                    {{--<div class="panel-body nopadmar">--}}
+                        {{--<div id="world-map-markers" style="width:100%; height:300px;"></div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
         </div>
     </section>
 
